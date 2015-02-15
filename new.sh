63ed3f4ad4acc044d3e7db211e66e6486ba0914e
Eric Budd <eric.b.budd@gmail.com>
2015-02-15 13:31:54 -0500

    Expanded new user script to automate common tasks

--- new.sh
+++ new.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+pwgen
 adduser $1 &&
 	mkdir /home/$1/public_html &&
 	mkdir /home/$1/.ssh &&
 	touch /home/$1/.ssh/authorized_keys &&
 	chown -R $1:$1 /home/$1 &&
 	make_mail.sh $1
+
+who_to_html.rb
